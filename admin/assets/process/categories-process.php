9ae0a7c498e11f2932c822c2c29a7c64a13638c1
jubair-k <jubairk576@gmail.com>
2022-04-11 21:10:44 +0530

    categories edit,delete,status changes

--- admin/assets/process/categories-process.php
+++ admin/assets/process/categories-process.php
@@ -20,10 +20,49 @@
 
         }
 
+        if(isset($_POST['categsavename']) && !empty($_POST['categsavename'])){
+            $chk_exist="SELECT * FROM categories WHERE categories=:categ AND id!=:categid";
+            $stmt=$pdo->prepare($chk_exist);
+            $stmt->execute(['categ'=>$_POST['categsavename'],'categid'=>$_POST['categsave']]);
+            $chk_count=$stmt->rowCount();
+            if($chk_count==0){
+                $updt_categ="UPDATE categories SET categories=:sacateg WHERE id=:scategid";
+                $stmt=$pdo->prepare($updt_categ);
+                $stmt->execute(['sacateg'=>$_POST['categsavename'],'scategid'=>$_POST['categsave']]);
+                $array="ok";    
+            }
+            else{
+                $array['exist']="category already exist";
+            }
+
+        }
+
         if(isset($_POST['loadCategories']) && !empty($_POST['loadCategories'])){
             $stmt=$pdo->query("SELECT * FROM categories");
             $array['categs']=$stmt->fetchAll();
         }
+
+        if(isset($_POST['deletecateg']) && !empty($_POST['deletecateg'])){
+            $dlt_categ="DELETE FROM categories WHERE id=:categid";
+            $stmt=$pdo->prepare($dlt_categ);
+            $stmt->execute(['categid'=>$_POST['categ']]);
+            $array="success";
+        }
+
+        if(isset($_POST['deactivecateg']) && !empty($_POST['deactivecateg'])){
+            $deact_categ="UPDATE categories SET status=:categst WHERE id=:categid";
+            $stmt=$pdo->prepare($deact_categ);
+            $stmt->execute(['categst'=>0,'categid'=>$_POST['categ']]);
+            $array="success";
+        }
+
+        if(isset($_POST['activecateg']) && !empty($_POST['activecateg'])){
+            $deact_categ="UPDATE categories SET status=:categst WHERE id=:categid";
+            $stmt=$pdo->prepare($deact_categ);
+            $stmt->execute(['categst'=>1,'categid'=>$_POST['categ']]);
+            $array="success";
+        }
+
 
         echo json_encode($array);
     }
--- admin/categories.php
+++ admin/categories.php
@@ -63,7 +63,7 @@
 
         <form class="board" id="addctegories_form">
             <label for="">Categories</label>
-            <input type="text" placeholder="Enter categories name" id="categoriesname" require>
+            <input type="text" placeholder="Enter categories name" id="categoriesname" autocomplete="off" require>
             <span class="exist_alert"></span>
             <input type="submit" class="submit" value="Submit">
         </form>
@@ -108,11 +108,19 @@
                 })
                 .then( res => res.json())
                 .then( data => {
-                    console.log(data);
+                    //console.log(data);
                     trs="";
                     for (const row of data.categs) {
-                        trs+=`<tr>
-                                <td>${row.categories}</td>
+                        trs+=`<tr data-id="${row.id}">
+                                <td>${row.categories}</td>`
+                        if(row.status=='1'){
+                            stbtn=`<button class="status_active stbtn">Active</button>`
+                        }else{
+                            stbtn=`<button class="status_deactive stbtn">Deactive</button>`
+                        }
+
+
+                        trs+=`<td class="operate_td">${stbtn} <button class="edite_categ">Edit</button> <button class="delete_categ">Delete</button></td>
                             </tr>
                         `
                     }
@@ -125,9 +133,85 @@
             $('#addctegories_form').on('submit',function(){
                 event.preventDefault();
                 if($('#categoriesname').val()!=""){
+                    if($('.submit').val()=="Submit"){
+                        let formData = new FormData();
+                        formData.append('addCategories','post');
+                        formData.append('categname',$('#categoriesname').val())
+                        fetch('assets/process/categories-process.php', {
+                            method: 'post',
+                            body: formData,
+                        })
+                        .then( res => res.json())
+                        .then( data => {
+                            //console.log(data);
+                            if(data=="ok"){
+                                $('#categoriesname').val('');
+                                loadCategories()
+                            }
+
+                            if(data.exist){
+                                $('.exist_alert').text(data.exist);
+                            }
+                            else{
+                                $('.exist_alert').text("");
+                            }
+                        })
+                    }
+
+                    if($('.submit').val()=="Save"){
+                        let formData = new FormData();
+                        formData.append('saveCategories','post');
+                        formData.append('categsavename',$('#categoriesname').val())
+                        formData.append('categsave',categedit_id)
+                        fetch('assets/process/categories-process.php', {
+                            method: 'post',
+                            body: formData,
+                        })
+                        .then( res => res.json())
+                        .then( data => {
+                            //console.log(data);
+                            if(data=="ok"){
+                                $('#categoriesname').val('');
+                                loadCategories()
+                                $('#addctegories_form').slideUp('fast');
+                                $('.submit').val('Submit')
+                            }
+
+                            if(data.exist){
+                                $('.exist_alert').text(data.exist);
+                            }
+                            else{
+                                $('.exist_alert').text("");
+                            }
+                        })
+                    }
+                }
+            })
+
+            $('body').on('click','.delete_categ',function(){
+                categ_id=this.parentElement.parentElement.dataset.id;
+                let formData = new FormData();
+                formData.append('deletecateg','post');
+                formData.append('categ',categ_id)
+                fetch('assets/process/categories-process.php', {
+                    method: 'post',
+                    body: formData,
+                })
+                .then( res => res.json())
+                .then( data => {
+                    //console.log(data);
+                    if(data=="success"){
+                        loadCategories()
+                    }
+                })
+            })
+
+            $('body').on('click','.stbtn',function(){
+                if(this.classList.contains('status_active')){
+                    categ_id=this.parentElement.parentElement.dataset.id;
                     let formData = new FormData();
-                    formData.append('addCategories','post');
-                    formData.append('categname',$('#categoriesname').val())
+                    formData.append('deactivecateg','post');
+                    formData.append('categ',categ_id)
                     fetch('assets/process/categories-process.php', {
                         method: 'post',
                         body: formData,
@@ -135,19 +219,38 @@
                     .then( res => res.json())
                     .then( data => {
                         //console.log(data);
-                        if(data=="ok"){
-                            $('#categoriesname').val('');
+                        if(data=="success"){
+                            loadCategories()
                         }
+                    })
+                }
 
-                        if(data.exist){
-                            $('.exist_alert').text(data.exist);
-                        }
-                        else{
-                            $('.exist_alert').text("");
+                if(this.classList.contains('status_deactive')){
+                    categ_id=this.parentElement.parentElement.dataset.id;
+                    let formData = new FormData();
+                    formData.append('activecateg','post');
+                    formData.append('categ',categ_id)
+                    fetch('assets/process/categories-process.php', {
+                        method: 'post',
+                        body: formData,
+                    })
+                    .then( res => res.json())
+                    .then( data => {
+                        //console.log(data);
+                        if(data=="success"){
+                            loadCategories()
                         }
                     })
                 }
             })
+
+            $('body').on('click','.edite_categ',function(){
+                categedit_id=this.parentElement.parentElement.dataset.id
+                $('#addctegories_form').slideDown('fast');
+                $('#categoriesname').val(this.parentElement.parentElement.children[0].textContent)
+                $('.submit').val('Save')
+            })
+
 
 
         })
