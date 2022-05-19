a30dc077d0926281dfd8dea7e4eeba46ac614533
jubair-k <jubairk576@gmail.com>
2022-05-19 08:53:40 +0530

    include header & footer

---
+++ header.php
@@ -0,0 +1,32 @@
+<section id="header">
+    <a href="#"><img src="assets/images/logo.png" class="logo" alt=""></a>
+    
+    <div>
+        <ul id="navbar">
+            <li><a class="page1 active" href="index.php">Home</a></li>
+            <li><a class="page2" href="shop.php">Shop</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a class="page3" href="about.html">About</a></li>
+            <li><a class="page4" href="contact.html">Contact</a></li>
+            <li id="lg-bag"><a href="cart.html"><i class="fal fa-shopping-bag"></i></a></li>
+            <a href="#" id="close"><i class="far fa-times"></i></a>
+        </ul>
+    </div>
+    
+    <div id="mobile">
+        <a href="cart.html"><i class="fal fa-shopping-bag"></i></a>
+        <i id="bar" class="fas fa-outdent"></i>
+
+    </div>
+    
+</section>
+
+<script>
+    //document.addEventListener('DOMContentLoaded',function(){
+        page=document.body.getAttribute('id');
+        currentActive=document.querySelector("#navbar li .active");
+        currentActive.classList.remove('active');
+        document.querySelector("."+page).classList.add('active');
+
+    //})
+</script>
--- index.php
+++ index.php
@@ -16,29 +16,8 @@
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Cara</title>
 </head>
-<body>
-    <section id="header">
-        <a href="#"><img src="assets/images/logo.png" class="logo" alt=""></a>
-        
-        <div>
-            <ul id="navbar">
-                <li><a class="active" href="index.html">Home</a></li>
-                <li><a href="shop.php">Shop</a></li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li id="lg-bag"><a href="cart.html"><i class="fal fa-shopping-bag"></i></a></li>
-                <a href="#" id="close"><i class="far fa-times"></i></a>
-            </ul>
-        </div>
-        
-        <div id="mobile">
-            <a href="cart.html"><i class="fal fa-shopping-bag"></i></a>
-            <i id="bar" class="fas fa-outdent"></i>
-
-        </div>
-    
-    </section>
+<body id="page1">
+    <?php include "header.php"; ?>
 
     <section id="hero">
         <h4>Trade-in-offer</h4>
@@ -197,55 +176,7 @@
         </div>
     </section>
 
-    <footer class="section-p1">
-        <div class="col">
-            <img class="logo" src="assets/images/logo.png" alt="">
-            <h4>Contact</h4>
-            <p><strong>Address: </strong> 562 Wellington Road, Street 32, San Francisco</p>
-            <p><strong>Phone: </strong> +01 2222 265 /(+91) 01 2345 5789</p>
-            <p><strong>Hours: </strong> 10:00 - 18:00, Mon - Sat</p>
-            <div class="follow">
-                <h4>Follow us</h4>
-                <div class="icon">
-                    <i class="fab fa-facebook-f"></i>
-                    <i class="fab fa-twitter"></i>
-                    <i class="fab fa-instagram"></i>
-                    <i class="fab fa-pinterest-p"></i>
-                    <i class="fab fa-youtube"></i>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <h4>About</h4>
-            <a href="#">About us</a>
-            <a href="#">Delivery Information</a>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms & Conditions</a>
-            <a href="#">Contact Us</a>
-        </div>
-        <div class="col">
-            <h4>My Account</h4>
-            <a href="#">Sign In</a>
-            <a href="#">View Cart</a>
-            <a href="#">My Wishlist</a>
-            <a href="#">Track My Order</a>
-            <a href="#">Help</a>
-        </div>
-        <div class="col install">
-            <h4>Install App</h4>
-            <p>From App Store or Doogle Play</p>
-            <div class="row">
-                <img src="assets/images/pay/app.jpg" alt="">
-                <img src="assets/images/pay/play.jpg" alt="">
-            </div>
-            <p>Secured Payment Gatways</p>
-            <img src="assets/images/pay/pay.png" alt="">
-        </div>
-
-        <div class="copyright">
-            <p>&copy; 2021, Cara shoping website</p>
-        </div>
-    </footer>
+    <?php include "footer.php"; ?>
 
 
     <script src="assets/js/script.js"></script>
